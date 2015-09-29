fda986153208e60311cba1bd73fa7bac22e5336a
Rafael Paiva <08.paiva@gmail.com>
2015-09-29 11:42:35 -0400

    Start modifing "file_lister" node to make it more powerful.

--- src/pumpkin_interface/src/file_lister.cpp
+++ src/pumpkin_interface/src/file_lister.cpp
@@ -2,7 +2,6 @@
 #include "file_type.h"
 #include "pumpkin_messages/Files.h"
 #include "std_msgs/String.h"
-#include <dirent.h>
 #include <boost/filesystem.hpp>
 
 using namespace pumpkin_messages;
@@ -29,98 +28,13 @@ void get_files_recursively(const bfs::path &dir, const std::string &ext, std::ve
 	}
 }
 
-bool getdir (std::string &dir, std::string &ftype, std::vector<std::string> &files)
-{
-    DIR *dp;
-    struct dirent *dirp;
-    if((dp  = opendir(dir.c_str())) == NULL) {
-        std::cout << "Error(" << errno << ") opening " << dir << std::endl;
-        return errno;
-    }
-
-    while ((dirp = readdir(dp)) != NULL) {
-        std::string s = dirp->d_name;
-		if (s.size() <= ftype.size())
-			continue;
-        if (s.compare (s.size() - ftype.size(), ftype.size(), ftype) == 0) {
-			//std_msgs::String s2;
-			//s2.data = s;
-            files.push_back(s);
-		}
-    }
-    closedir(dp);
-    return 0;
-}
-
-/*bool ListSerialPorts(pumpkin_interface::Serials::Request &req, pumpkin_interface::Serials::Response &res)
-{
-	FILE *pipe = popen("setserial -g /dev/ttyACM*", "r");
-	char buffer[64];
-    std::string console = "";
-    while(!feof(pipe)) {
-    	if(fgets(buffer, 64, pipe) != NULL)
-    		console += buffer;
-    }
-
-	pclose(pipe);
-
-	//Separate ports into a vector of strings
-	
-	std::string buf2;
-	std::set<std::string> bag;
-	for (std::string::iterator it = console.begin(); it != console.end(); ++it) 
-	{
-		if (*it != '\n')
-			buf2 += *it;
-		else
-		{
-			bag.insert(buf2);
-			buf2.clear();
-		}
-	}
-
-	for (std::set<std::string>::iterator it = bag.begin(); it != bag.end(); ++it)
-	{
-		size_t pos;
-		if ((pos = (*it).find("UART")) == std::string::npos)	//If not found UART
-			continue;
-
-		const std::string &port = (*it).substr(0, pos-2);
-		
-
-		//string comm = "rosrun rosserial_python serial_node.py ";
-		//system((comm+(*it)).c_str());
-
-		
-
-		res.terminals.push_back(port);
-	}
-
-	return true;
-}*/
-
 bool ListFiles (Files::Request &req, Files::Response &res)
 {
-	/*
-	//Get pumpkin base folder
-    FILE *pipe = popen("rospack find pumpkin", "r");
-    if (!pipe) return false;
-    char buffer[128];
-    std::string folder = "";
-    std::string extension;
-    while(!feof(pipe)) {
-    	if(fgets(buffer, 128, pipe) != NULL)
-    		folder += buffer;
-    }
-	//try to remove a possible \n
-	folder = folder.substr(0, folder.size()-1);
-	*/
-
 	std::string folder = pumpkin_path;
 	std::string extension;
     
-    //It doesn't look like so Object Oriented, but...
-    switch(FileType(req.filetype)) {
+
+    switch(FileType(req.type)) {
         case FileType::ConfigFile:
             folder += "/config";
             extension = ".yaml";
@@ -139,15 +53,9 @@ bool ListFiles (Files::Request &req, Files::Response &res)
             return false;
     }
 
-	/*
-    //Here, we can, hereafter, make a rosout showing an error code
-    if (getdir(folder, extension, res.filenames) != 0)
-        return false;
-        */
+	get_files_recursively(bfs::path(folder), extension, res.files);
 
-    res.folder = folder;
-
-    return true;
+    return res.files.size() > 0;
 }
 
 int main (int argc, char** argv)
@@ -165,7 +73,6 @@ int main (int argc, char** argv)
 	pumpkin_path = env;
 
     ros::ServiceServer files = n.advertiseService("file_lister", ListFiles);
-	ros::ServiceServer ports = n.advertiseService("port_lister", ListSerialPorts);
     ROS_INFO("Ready.");
     ros::spin();
 
