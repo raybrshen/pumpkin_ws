424d097131d0ec988d17e46d3a9ef5e1e185fab3
Rafael Paiva <08.paiva@gmail.com>
2015-09-29 12:30:42 -0400

    file_type is working.

--- src/pumpkin_interface/src/file_lister.cpp
+++ src/pumpkin_interface/src/file_lister.cpp
@@ -6,19 +6,26 @@
 using namespace pumpkin_messages;
 namespace bfs = boost::filesystem;
 
+
 std::string pumpkin_path;
 
 void get_files_recursively(const bfs::path &dir, const std::string &ext, std::vector<FileList> &found) {
 	FileList files;
 	if (!bfs::exists(dir))
 		return;
-	for (bfs::directory_iterator it(dir), end_it; it != end_it; ++it) {
+	if (!bfs::is_directory(dir))
+		return;
+	bfs::directory_iterator end_it;
+	for (bfs::directory_iterator it(dir); it != end_it; ++it) {
 		if (bfs::is_directory(it->path())) {
 			get_files_recursively(it->path(), ext, found);
 		} else {
-			const std::string &filename = it->path().leaf().string();
-			if (filename.compare(filename.size() - ext.size(), ext.size(), ext) == 0)
-				files.filenames.push_back(filename);
+			const std::string filename = it->path().leaf().string();
+			//ROS_INFO("Filename: %s", filename.c_str());
+			if (filename.size() > ext.size())
+				if (filename.compare(filename.size() - ext.size(), ext.size(), ext) == 0)
+					files.filenames.push_back(filename);
+
 		}
 	}
 	if (files.filenames.size() > 0) {
@@ -33,7 +40,7 @@ bool ListFiles (Files::Request &req, Files::Response &res)
 	std::string extension;
     
 
-    switch(FileType(req.type)) {
+    switch(static_cast<FileType>(req.type)) {
         case FileType::ConfigFile:
             folder += "/config";
             extension = ".yaml";
@@ -51,6 +58,7 @@ bool ListFiles (Files::Request &req, Files::Response &res)
             return false;
     }
 
+	ROS_INFO("Folder: %s", folder.c_str());
 	get_files_recursively(bfs::path(folder), extension, res.files);
 
     return res.files.size() > 0;
@@ -59,7 +67,7 @@ bool ListFiles (Files::Request &req, Files::Response &res)
 int main (int argc, char** argv)
 {
 
-    ros::init(argc, argv, "lister_server");
+    ros::init(argc, argv, "file_lister");
     ros::NodeHandle n;
 
 	char * env;
